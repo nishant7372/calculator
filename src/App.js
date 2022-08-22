e4ffa954c67ac6c0b10a43f79ca73f07ae91d8e6
NishantSingh02 <nst7372@gmail.com>
2022-08-22 16:40:13 +0530

    final

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Calculator from "./Components/Calculator";
 function App() {
   return (
     <div className="App">
-      <Calculator />
+      <div className="page">
+        <Calculator />
+      </div>
     </div>
   );
 }

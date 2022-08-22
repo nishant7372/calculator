513a9733e888f8120ae1fefa8f2cc8aa525f9212
NishantSingh02 <nst7372@gmail.com>
2022-08-22 16:02:31 +0530

    Final Commit

--- src/Components/NumberGrid.js
+++ src/Components/NumberGrid.js
@@ -7,19 +7,19 @@ export default function NumberGrid({ selectNum }) {
       <CalcButton num="7" clr="black-btn" selectNum={selectNum} />
       <CalcButton num="8" clr="black-btn" selectNum={selectNum} />
       <CalcButton num="9" clr="black-btn" selectNum={selectNum} />
-      <CalcButton num="/" clr="white-btn" selectNum={selectNum} />
+      <CalcButton num="/" clr="blue-btn" selectNum={selectNum} />
       <CalcButton num="4" clr="black-btn" selectNum={selectNum} />
       <CalcButton num="5" clr="black-btn" selectNum={selectNum} />
       <CalcButton num="6" clr="black-btn" selectNum={selectNum} />
-      <CalcButton num="x" clr="white-btn" selectNum={selectNum} />
+      <CalcButton num="x" clr="blue-btn" selectNum={selectNum} />
       <CalcButton num="1" clr="black-btn" selectNum={selectNum} />
       <CalcButton num="2" clr="black-btn" selectNum={selectNum} />
       <CalcButton num="3" clr="black-btn" selectNum={selectNum} />
-      <CalcButton num="-" clr="white-btn" selectNum={selectNum} />
+      <CalcButton num="-" clr="blue-btn" selectNum={selectNum} />
       <CalcButton num="0" clr="black-btn" selectNum={selectNum} />
       <CalcButton num="ce" clr="orange-btn" selectNum={selectNum} />
-      <CalcButton num="=" clr="white-btn" selectNum={selectNum} />
-      <CalcButton num="+" clr="white-btn" selectNum={selectNum} />
+      <CalcButton num="=" clr="blue-btn" selectNum={selectNum} />
+      <CalcButton num="+" clr="blue-btn" selectNum={selectNum} />
     </div>
   );
 }

a9c8a4f5c2a9d80078729a8a8c45ad9c8d76eaf3
NishantSingh02 <nst7372@gmail.com>
2023-05-09 01:41:40 +0530

    github link added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import Calculator from "./Components/Calculator";
 function App() {
   return (
     <div className="App">
+      <div className={"github"}>
+        <a
+          className="fa-brands fa-github"
+          href="https://github.com/nishant7372/calculator"
+        >
+          {""}
+        </a>
+      </div>
       <Calculator />
     </div>
   );
